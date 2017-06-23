83c147e8e3ee151f167d77d8afd9836016677a60
produgraf <produgraf2000@gmail.com>
2017-06-23 10:12:58 -0300

    fdd

--- morse.py
+++ morse.py
@@ -1,6 +1,10 @@
 
 class codigoMorse(object):
-    morse = {'a': '.-', 'b' : '-...' , 'c' : '-.-.' , 'd' : '-..' , 'e' : '.' , 'f' : '..-.' , 'g' : '--.' , 'h' : '....' , 'i' : '..' , 'j' : '.---' , 'k' : '-.-' , 'l' : '.-..' , 'm' : '--' , 'n' : '-.' , 'o' : '---' , 'p' : '.--.' , 'q' : '--.-' , 'r' : '.-.' , 's' : '...' , 't' : '-' , 'u' : '..-' , 'v' : '...-' , 'w' : '.--' , 'x' : '-..-' , 'y' : '-.--' , 'z' : '--..','1': '.----','2':'..---','3':'...--', '4' : '....-', '5' : '.....' , '6' : '-....' , '7' : '--...' , '8' : '---..' , '9' : '----.' , '0' : '-----', ' ' : '___'}
+    morse = {'a' : '.-'    , 'b' : '-...' ,  'c' : '-.-.' ,  'd' : '-..' ,  'e' : '.' ,   'f' : '..-.' , 'g' : '--.' ,   'h' : '....' ,
+             'i' : '..'    , 'j' : '.---' ,  'k' : '-.-' ,   'l' : '.-..' , 'm' : '--' ,  'n' : '-.' ,   'o' : '---' , 'p' : '.--.' ,
+             'q' : '--.-'  , 'r' : '.-.' ,   's' : '...' ,   't' : '-' ,    'u' : '..-' , 'v' : '...-' , 'w' : '.--' , 'x' : '-..-' ,
+             'y' : '-.--'  , 'z' : '--..' ,  '1' : '.----',  '2' : '..---', '3':'...--',  '4' : '....-', '5' : '.....' , '6' : '-....' ,
+             '7' : '--...' , '8' : '---..' , '9' : '----.' , '0' : '-----', ' ' : '___'}
 
     def agregarLetra(self, letra, codigo):
         self.morse[letra] = codigo
@@ -8,7 +12,12 @@ class codigoMorse(object):
     def eliminarLetra(self, letra):
         del self.morse[letra]
 
-    def traducirTexto(self, texto):
+
+    def traducirTexto(self,texto):
         oracion = ""
         for letra in texto:
-            oracion = oracion + self.morse[letra]
+            if letra in self.morse[letra]:
+                oracion = oracion
+            else:
+                oracion = oracion + self.morse[letra] + "|"
+        return oracion
