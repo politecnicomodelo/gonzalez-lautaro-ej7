238f95dc39050e5ccda076c8478ab09edd4503fd
produgraf <produgraf2000@gmail.com>
2017-06-30 11:37:06 -0300

    fdd

--- prueba.py
+++ prueba.py
@@ -1,5 +1,11 @@
 from clases.pruebaMorse import codigoMorse
 escrito = ""
 codigo = codigoMorse()
-escrito = codigo.traducirMorse("....|---|.-..|.-|")
+opcion = 0
+opcion = input("1: texto a morse, 2: morse a texto ")
+escrito = input("ingrese el texto: ")
+if (opcion == "1"):
+    escrito = codigo.traducirTexto(escrito)
+elif(opcion == "2"):
+    escrito = codigo.traducirMorse(escrito)
 print(escrito)
